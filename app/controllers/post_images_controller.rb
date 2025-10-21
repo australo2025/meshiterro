73fa8b6c5a1640eb7b9a4901ab03da4e0d26c437
australo2025 <225503601+australo2025@users.noreply.github.com>
2025-10-21 11:39:19 +0000

    テスト

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,37 @@
+class PostImagesController < ApplicationController
+
+  def new
+    @post_image = PostImage.new
+  end
+  
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  def index
+    @post_images = PostImage.page(params[:page])
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
+  end
+
+  def destroy
+    post_image = PostImage.find(params[:id])  # 削除する投稿を特定
+    post_image.destroy                        # 投稿を削除
+    redirect_to post_images_path              # 投稿一覧ページへリダイレクト
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
+end
